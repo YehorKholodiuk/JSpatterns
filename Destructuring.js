b883001e2b3d1ad3eeea91c42eb218f3945daa42
ASUS <Yehor Kholodiuk>
2021-12-14 20:26:03 +0200

    initial commit

---
+++ Destructuring.js
@@ -0,0 +1,7 @@
+let person = {name: "Yehor", country: "Ukraine", job: "Developer"};
+
+let {name, country, job} = person;
+
+console.log(name);//"Yehor"
+console.log(country);//"Ukraine"
+console.log(job);//Developer"
